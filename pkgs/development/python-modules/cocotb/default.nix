0a86d663029b7cb07b742d7ff2063297d515a9da
Matt Huszagh <huszaghmatt@gmail.com>
2019-10-17 08:25:47 -0700

    Fix cocotb tests

--- pkgs/development/python-modules/cocotb/default.nix
+++ pkgs/development/python-modules/cocotb/default.nix
@@ -1,19 +1,38 @@
-{ stdenv, buildPythonPackage, fetchPypi, setuptools }:
+{ stdenv, buildPythonPackage, fetchFromGitHub, setuptools, swig, verilog }:
 
 buildPythonPackage rec {
   pname = "cocotb";
   version = "1.2.0";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0855gs1wi7k9fwk832czri21wr83gzp0lp2jkaxccyjgbr1d8a8z";
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "091q63jcm87xggqgqi44lw2vjxhl1v4yl0mv2c76hgavb29w4w5y";
   };
 
   propagatedBuildInputs = [
     setuptools
   ];
 
-  doCheck = false;
+  postPatch = ''
+    patchShebangs bin/*.py
+
+    # POSIX portability (TODO: upstream this)
+    for f in \
+      cocotb/share/makefiles/Makefile.* \
+      cocotb/share/makefiles/simulators/Makefile.*
+    do
+      substituteInPlace $f --replace 'shell which' 'shell command -v'
+    done
+  '';
+
+  checkInputs = [ swig verilog ];
+
+  installCheckPhase = ''
+    export PATH=$out/bin:$PATH
+    make test
+  '';
 
   meta = {
     description = "Coroutine based cosimulation library for writing VHDL and Verilog testbenches in Python";
